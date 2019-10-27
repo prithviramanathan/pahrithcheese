aaa3fcf87e65ec09bf6b112885d8385727cb823e
Prithvi <prithvi.ramanathan@gmail.com>
2019-10-27 16:12:20 -0500

    hello

--- scrapingtools/spiders/cheese.py
+++ scrapingtools/spiders/cheese.py
@@ -7,15 +7,15 @@ def cleanhtml(raw_html):
   cleantext = re.sub(cleanr, '', raw_html)
   return cleantext
 
+cheeses = 'parmesan gouda cheddar feta mozzarella brie pepper-jack'
+cheese_array = cheeses.split()
+
 class CheeseSpider(scrapy.Spider):
 	name = "scrapingtools"
 
 	def start_requests(self):
-		urls = [
-			'http://cheese.com/parmesan',
-			'http://cheese.com/cheddar'
-		]
-		for url in urls:
+		for cheese in cheese_array:
+			url = 'http://cheese.com/' + cheese
 			yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
@@ -35,43 +35,25 @@ class CheeseSpider(scrapy.Spider):
                     break
 	    other_info = response.xpath('//p').extract()
 	    for item in other_info:
-		text = cleanhtml(item)
-		if 'Region' in text:
-		    words = text.split(' ')[1]
-                    cheese['region'] = ' '.join(words[1:])
-		if 'Family' in text:
-		    cheese['family'] = text.split(' ')[1]
-		"""
-		name_path = response.url.split("/")[-1].split(".")[0]
-		pic_url = 'http://senate.ontheissues.org/pictures/' + name_path + '.jpg'
-
-		politician = PoliticianItem(name=pol_info[1].rstrip(), office=pol_info[0], pic_url=pic_url)
-
-		tables = response.xpath('//ul')
-		names = response.xpath('//td[@align="center"]/font[@face="Arial"]/text()').extract()
-		issues = list()
-		for x in range(0, len(names)):
-			#print("hey:", tables[x])
-			items = tables[x].xpath('li/text()').extract()
-			#print(items)
-			# cool = tables[x].xpath('li')
-			# print(cool)
-			new_items = list()
-			skip = False
-			for item in items:
-				if "Rated" in item:
-					skip = True
-				elif not skip:
-					new_items.append(item)
-				else:
-					skip = False
-			print(new_items)
-
-			issue = IssueItem(issue=names[x].rstrip(), stances=new_items)
-			issues.append(dict(issue))
-
-		politician['issues'] = issues
-		yield politician
-		"""
+			text = cleanhtml(item)
+			words = text.split(':')[1]
+			if 'Region' in text:
+                cheese['region'] = words
+			if 'Family' in text:
+		    	cheese['family'] = words
+			if 'Fat content' in text:
+				cheese['fat_content'] = words
+			if 'Colour' in text:
+				cheese['colour'] = words
+			if 'Flavour' in text:
+				cheese['flavour'] = words
+			if 'Aroma' in text:
+				cheese['aroma'] = words
+			if 'Texture' in text:
+				cheese['texture'] = words
+			if 'Country of origin' in text:
+				cheese['country_of_origin'] = words
+			if 'Type' in text:
+				cheese['type'] = words
 		yield cheese
                 return
