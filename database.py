824b5cb33a90e65d4628f48972d788cba9ce10cd
EC2 Default User <ec2-user@ip-172-31-47-93.us-east-2.compute.internal>
2019-10-28 04:50:25 +0000

    more database setup

--- database.py
+++ database.py
@@ -3,14 +3,13 @@ import mysql.connector
 def create_database(cursor):
     cursor.execute("CREATE DATABASE pahrithcheese")
 
+def create_cheese_table(cursor):
+
 if __name__ == '__main__':
     my_db = mysql.connector.connect(
-        host='127.0.0.1',
-        user='pahrithcheese',
-        passwd='Pahrithcheese!'
-    )
+        user='root', password='Pahrithcheese!', database='pahrithcheese')
+    print('connected')
     my_cursor = my_db.cursor()
-    create_database(my_cursor)
     my_cursor.execute("SHOW DATABASES")
 
     for x in my_cursor:
