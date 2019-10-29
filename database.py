f7f179242fd468f43319714ecf96244ce2d7cd51
Prithvi <prithvi.ramanathan@gmail.com>
2019-10-29 00:35:35 -0500

    more changes

--- database.py
+++ database.py
@@ -1,5 +1,23 @@
 import mysql.connector
 import json
+def Cheese():
+    def __init__(self, item):
+        self.name = item[0]
+        self.description = item[1]
+        self.region = item[3]
+        self.family = item[4]
+        self.fat_content = item[5]
+        self.color = item[6]
+        self.flavor = item[7]
+        self.aroma = item[8]
+        self.texture = item[9]
+        self.type = item[10]
+        self.country_of_origin = item[11]
+        self.image = item[12]
+        self.recipe = item[13]
+
+
+
 def create_database(cursor):
     cursor.execute("CREATE DATABASE pahrithcheese")
 
@@ -14,7 +32,6 @@ def create_pairings_table(cursor):
 
 def fill_cheese_table(cursor, db):
     sql = "INSERT INTO cheeses (name, description, region, family, fat_content, color, flavor, aroma, texture, type, country_of_origin, image) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    vals = []
     data = None
     with open('cheese.json') as f:
         data = json.load(f)
@@ -26,7 +43,32 @@ def fill_cheese_table(cursor, db):
         val = (item.get('name', None), item.get('description', None), item.get('region', None), item.get('family', None), item.get('fat_content', None), item.get('colour', None), item.get('flavour', None), item.get('aroma', None), item.get('texture', None), item.get('type', None), item.get('country_of_origin', None), item.get('image', None))
         cursor.execute(sql, val)
         db.commit()
-        print('inserted an item')                                                               
+
+def fill_pairings_table(cursor, db):
+    sql = "INSERT INTO pairings (name, recipe) VALUES (%s, %s)"
+    data = None
+    with open('cheese.json') as f:
+        data = json.load(f)
+    cheeses = set([])
+    for item in data:
+        if item['name'] in cheeses:
+            continue
+        cheeses.add(item['name'])
+        val = (item.get('name', None), '')
+        cursor.execute(sql, val)
+        db.commit()
+
+def search_cheeses(cursor, cheese):
+    sql_string = 'SELECT * FROM cheeses NATURAL JOIN pairings WHERE name = "' + cheese + '"'
+    cursor.execute(sql_string)
+    result = cursor.fetchone()
+    return Cheese(result)
+
+def update_pairings(cursor, cheese, recipe):
+    sql_string = "UPDATE pairings SET recipe = '" + recipe + "' WHERE name = '" + cheese + "' AND recipe ='" + recipe + "'"
+    cursor.execute(sql_string)
+    cursor.commit()
+
 if __name__ == '__main__':
     my_db = mysql.connector.connect(
         user='root', password='Pahrithcheese!', database='pahrithcheese')
@@ -34,7 +76,8 @@ if __name__ == '__main__':
     #create_cheese_table(my_cursor)
     #create_pairings_table(my_cursor)
     #fill_cheese_table(my_cursor, my_db)
+    fill_pairings_table(my_cursor, my_db)
     #print('inserted elements')
-    my_cursor.execute('SELECT COUNT(*) FROM cheeses')
+    my_cursor.execute('SELECT COUNT(*) FROM pairings')
     for x in my_cursor:
         print(x)
