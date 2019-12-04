a01393362e175b1aedf137ec926dd206afc07549
nrao64 <nickrao64@gmail.com>
2019-12-04 16:44:27 -0600

    minor ui fixes

--- pahrithcheese-web/src/app/cheese-search/cheese-search.component.ts
+++ pahrithcheese-web/src/app/cheese-search/cheese-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CheeseApiService } from '../services/cheese-api.service';
+import { ToastrService } from 'ngx-toastr';
 
 export interface Cheese {
   country_of_origin: string;
@@ -25,10 +26,9 @@ export interface Cheese {
 export class CheeseSearchComponent implements OnInit {
   cheeseData: Cheese;
 
-  constructor(private cheeseService: CheeseApiService) { }
+  constructor(private cheeseService: CheeseApiService, private toastrService: ToastrService) { }
 
   ngOnInit() {
-    this.cheeseData = {"country_of_origin": "Italy", "pairing": "", "fat_content": "25.83  g/100g", "name": "Parmesan", "family": "Parmesan", "region": "Provinces of Parma, Reggio Emilia, Modena, Bologna , Mantua", "texture": "crystalline, dense and grainy", "aroma": "strong", "image": "https://www.cheese.com/media/img/cheese/Parmesan_1.jpg", "flavor": "fruity, nutty, savory, sharp", "type": "hard, artisan", "color": "straw", "description": "The Parmigiano Reggiano or Parmesan cheese as it is called in English is considered to be among the top cheeses by cheese connoisseurs. Today, it is produced by various producers. However, PDO designation states that for a cheese to be called as Parmesan, it has to be produced from cows grazing on fresh grass and hay.\r\nCheeses mocking Parmigiano Reggiano are called as Parmesan or Italian hard cheese by producers to avoid legal issues.&amp;nbsp;Parmigiano Reggiano cheese is named after the provinces in which it is made, namely Provinces of Parma, Reggio Emilia, Modena, Bologna and Mantua.\r\nTrue Parmesan cheese has a hard, gritty texture and is fruity and nutty in taste. Cheeses mocking Parmesan or inferior Parmesan may have a bitter taste. Parmigiano Reggiano cheese is mostly grated over pastas, used in soups and risottos. It is also eaten on its own as a snack."};
   }
 
   populateFields(cheese: string) {
@@ -39,7 +39,7 @@ export class CheeseSearchComponent implements OnInit {
 
   addPairing(cheese: string, pairing: string) {
     this.cheeseService.updatePairing(cheese, pairing).subscribe((data) => {
-      console.log(data);
+      this.toastrService.success('Updated pairing!');
     });
   }
 
