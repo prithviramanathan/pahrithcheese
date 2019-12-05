22a77ef3fbbc8f55105f85b6399c1dc4bec569ab
EC2 Default User <prithvi.ramanathan@gmail.com>
2019-12-05 08:20:28 +0000

    working mongodb connection

--- mongodb.py
+++ mongodb.py
@@ -1,18 +1,17 @@
 from pymongo import MongoClient
+from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb')
+client = MongoClient(port=27017)
 db=client.pahrithcheese
 
-c = "gouda"
+c = "Gouda"
 
 result = db.available.find({ "cheese_name": c }, {"_id":0, "store_id": 1})
 
-
 storeList = []
-
 for r in result:
-     storeList.append(r[store_id])
+     storeList.append(r["store_id"])
 
-final = db.stores.find({ "store_id": {"$in": storeList} }, { "_id": 0, "store_name": 1, "address": 1})
+final = list(db.stores.find({ "store_id": {"$in": storeList} }, { "_id": 0, "store_name": 1, "address": 1}))
 
 pprint(final)
