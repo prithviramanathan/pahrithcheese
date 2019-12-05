65183d332187edacf4b1e5ad6da185786e9f9c0f
Darius Haery <dariushaery@Dariuss-MacBook-Pro.local>
2019-12-05 01:19:07 -0600

    added query for mango

--- mongodb.py
+++ mongodb.py
@@ -3,3 +3,16 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb')
 db=client.pahrithcheese
 
+c = "gouda"
+
+result = db.available.find({ cheese_name: c }, {_id:0, store_id: 1})
+
+
+storeList = []
+
+for r in result:
+     storeList.append(r[store_id])
+
+final = db.stores.find({ store_id: {$in: storeList} }, { _id: 0, store_name: 1, address: 1})
+
+print(final)
